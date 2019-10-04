a8bb16efcddaffbac81883d47243f50f170d445a
mans122 <gksqufdhkdwk2@gmail.com>
2019-10-04 16:44:47 +0900

    학사관리프로그램 10-04

--- Haksa/src/Haksa.java
+++ Haksa/src/Haksa.java
@@ -2,51 +2,58 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Haksa extends JFrame {
+	//학번~주소를 입력받을 TextField 4개 선언
 	public static JTextField[] tf_num = new JTextField[4];
+	//목록을 출력할 TextArea 선언
+	public static JTextArea taList=new JTextArea(15,27);
+	
 	public Haksa() {
 		setTitle("학사관리");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout(FlowLayout.LEFT));
 		
-		//20크기 텍스트필드 4개 생성
+		//24크기 텍스트필드 4개 생성
 		for(int i=0;i<4;i++) {tf_num[i] = new JTextField(24);}
 		c.add(new JLabel("학번"));
 		c.add(tf_num[0]);
 		c.add(new JLabel("이름"));
 		c.add(tf_num[1]);
-		c.add(new JLabel("학번"));
+		c.add(new JLabel("학과"));
 		c.add(tf_num[2]);
 		c.add(new JLabel("주소"));
 		c.add(tf_num[3]);
-		
-		JTextArea taList=new JTextArea(15,28);
+		//TextArea를 taList라는 이름으로 생성 후 크기 지정,
+		//패널에 등록
 		JScrollPane sp = new JScrollPane(taList);
 		c.add(sp);
 		
-		//버튼생성 및 리스너등록 ,패널 등록
+		//버튼생성
 		JButton btnInsert = new JButton("등록");
 		JButton btnList = new JButton("목록");
 		JButton btnUpdate = new JButton("수정");
 		JButton btnDelete = new JButton("삭제");
 		MyActionListener ma = new MyActionListener();//리스너 생성
+		//버튼을 리스너에 등록
 		btnInsert.addActionListener(ma);
 		btnList.addActionListener(ma);
 		btnUpdate.addActionListener(ma);
 		btnDelete.addActionListener(ma);
+		//버튼들 크기조절
 		btnInsert.setPreferredSize(new Dimension(74,30));
 		btnList.setPreferredSize(new Dimension(74,30));
 		btnUpdate.setPreferredSize(new Dimension(74,30));
 		btnDelete.setPreferredSize(new Dimension(74,30));
+		//버튼을 패널에 등록
 		c.add(btnInsert);
 		c.add(btnList);
 		c.add(btnUpdate);
 		c.add(btnDelete);
 
 		taList.setEditable(false);
-		taList.append("============================================\n");
-		taList.append("  학번	이름	학과	주소\n");
-		taList.append("============================================\n");
+		taList.append("==========================================\n");
+		taList.append("학번\n");
+		taList.append("==========================================\n");
 
 		setSize(335,480);
 		setVisible(true);
--- Haksa/src/MyActionListener.java
+++ Haksa/src/MyActionListener.java
@@ -1,40 +1,111 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.event.*;
+import java.sql.*;
 import javax.swing.JOptionPane;
 
 public class MyActionListener implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
+		//입력받는 액션의 이름을 String형태의 변수 cmd에 저장
 		String cmd = e.getActionCommand();
+
+		//JDBC 연결을 위한 변수 선언
+		Connection conn=null;
+		Statement stmt=null;
+		ResultSet rs = null;
+		String url = "jdbc:oracle:thin:@localhost:1521:myoracle";
+		String uid = "ora_user";
+		String pass = "hong";
+		//값비었는지 확인하기위한 value값 변수 선언
 		int value = 0;
+		//등록시 값을 받아오기위한 변수선언
+		String id = new String();
+		String name = new String();
+		String dept = new String();
+		String address = new String();
+
 		switch(cmd) {
 		case "등록":
 			for(int i=0;i<4;i++) {//tf_num값 비어있는게 있는지 확인
-				if(Haksa.tf_num[i].getText().length()==0 || Haksa.tf_num[i].getText().equals("")) {
+			if(Haksa.tf_num[i].getText().length()==0 || Haksa.tf_num[i].getText().equals("")) {
 					value = 1;//비었으면 value값을 1로 설정
 				}
 			}
-			if(value==1) {//비었으면 메시지 출력
-				JOptionPane.showMessageDialog(null,"값이 입력되지 않았습니다.","경고",JOptionPane.WARNING_MESSAGE);
-			}
-			else {
-				System.out.println("등록");
+			if (value != 1){
+				try {
+					//학번,이름,학과,주소를 순서대로 변수에 넣어줌
+					id = Haksa.tf_num[0].getText();
+					name = Haksa.tf_num[1].getText();
+					dept = Haksa.tf_num[2].getText();
+					address = Haksa.tf_num[3].getText();
+					//JDBC연결
+					Class.forName("oracle.jdbc.driver.OracleDriver");
+					conn=DriverManager.getConnection(url,uid,pass);
+					System.out.println("연결완료");//연결 잘 됬는지 확인코드
+					stmt=conn.createStatement();
+					
+					//stmt.executeQuery("insert into student values("+id+","+name+","+dept+","+address+")");
+					stmt.executeQuery("insert into student values('"+id+"','"+name+"','"+dept+"','"+address+"')");
+					
+					System.out.println(id);
+					System.out.println(name);
+					System.out.println(dept);
+					System.out.println(address);
+				}
+				
+				
+				
+				catch(Exception inputE) {
+					inputE.printStackTrace();
+				}
 				for(int i=0;i<4;i++) {
 					Haksa.tf_num[i].setText("");
 				}
 			}
+			else {//비었으면 메시지 출력
+				JOptionPane.showMessageDialog(null,"값이 입력되지 않았습니다.","경고",JOptionPane.WARNING_MESSAGE);
+				break;
+			}
 			break;
+//--------------------------------------------------------------			
 		case "목록":
+			try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				conn=DriverManager.getConnection(url,uid,pass);
+				System.out.println("연결완료");
+				stmt=conn.createStatement();
+				rs = stmt.executeQuery("select * from student");
+				
+				while(rs.next()) {
+					
+					//Haksa.taList.append(rs.getString("id"));
+					Haksa.taList.append(rs.getString("id")); //칼럼 이름대신 칼럼 인덱스도 사용 가능
+					Haksa.taList.append("\t     ");
+					Haksa.taList.append(rs.getString("name"));
+					Haksa.taList.append("\t ");
+					Haksa.taList.append(rs.getString("dept"));
+					Haksa.taList.append("\t ");
+					Haksa.taList.append(rs.getString("address"));
+					Haksa.taList.append("\n");
+				}
+				rs.close();
+				conn.close();
+			} catch (Exception listE) {
+				listE.printStackTrace();
+			}
 			System.out.println("목록");
 			break;
+//--------------------------------------------------------------
 		case "수정":
 			System.out.println("수정");
 			break;
+//--------------------------------------------------------------			
 		case "삭제":
 			if(JOptionPane.showConfirmDialog(null, "정말삭제하시겠습니까?", "삭제", JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION) 
 				System.out.println("삭제처리");
 			break;
+
+
 		}
+
 	}
-	
+
 }
